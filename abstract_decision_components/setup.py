034aa54dff515355564e88002295787ee9a751ea
Tyler <t.k.olson@student.tudelft.nl>
2025-05-12 16:03:12 +0200

    added new aggregate nodes

--- abstract_decision_components/setup.py
+++ abstract_decision_components/setup.py
@@ -20,25 +20,27 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'accept_always = abstract_decision_components.accept.accept_always_node:main',
-            'accept_size = abstract_decision_components.accept.accept_size_node:main',
-            'accept_satisficing = abstract_decision_components.accept.accept_satisficing_node:main',
-            'accept_dominating = abstract_decision_components.accept.accept_dominating_node:main',
+            'cue_prolog = abstract_decision_components.cue.cue_prolog_node:main',
 
-            'take_best = abstract_decision_components.take.take_best_node:main',
-            'eliminate_worst = abstract_decision_components.take.eliminate_worst_node:main',
+            'assess = abstract_decision_components.assess.assess_node:main',
+
+            'aggregate_preferences = abstract_decision_components.aggregate.aggregate_preferences_node:main',
+            'aggregate_utility_boolean = abstract_decision_components.aggregate.aggregate_utility_boolean_node:main',
+            'aggregate_utility_signed = abstract_decision_components.aggregate.aggregate_utility_signed_node:main',
+            'aggregate_utility_sum = abstract_decision_components.aggregate.aggregate_utility_sum_node:main',
+            'aggregate_multi_value_utility = abstract_decision_components.aggregate.aggregate_multi_value_utility_node:main',
 
             'order_condorcet_extension = abstract_decision_components.order.order_condorcet_extension_node:main',
             'order_lexicographical = abstract_decision_components.order.order_lexicographical_node:main',
             'order_dominating = abstract_decision_components.order.order_dominating_node:main',
 
-            'aggregate_preferences = abstract_decision_components.aggregate.aggregate_preferences_node:main',
-            'aggregate_utility = abstract_decision_components.aggregate.aggregate_utility_node:main',
-            # 'aggregate_multi_value_utility = abstract_decision_components.aggregate.aggregate_multi_value_utility_node:main',
+            'take_best = abstract_decision_components.take.take_best_node:main',
+            'eliminate_worst = abstract_decision_components.take.eliminate_worst_node:main',
 
-            'assess = abstract_decision_components.assess.assess_node:main',
-
-            'cue_prolog = abstract_decision_components.cue.cue_prolog_node:main',
+            'accept_always = abstract_decision_components.accept.accept_always_node:main',
+            'accept_size = abstract_decision_components.accept.accept_size_node:main',
+            'accept_satisficing = abstract_decision_components.accept.accept_satisficing_node:main',
+            'accept_dominating = abstract_decision_components.accept.accept_dominating_node:main',
         ],
     },
 )
