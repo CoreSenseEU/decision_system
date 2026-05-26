34ed8596bc867fa57bb2ec3cd9b2f0dc0c06cdb8
Alex Gabriel <einalex@4d6.de>
2026-05-26 22:54:49 +0200

    added BT dependency

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 export DEBIAN_FRONTEND=noninteractive
 sudo apt-get update
 sudo apt-get install -y python3-pip swi-prolog
-pip install --user --break-system-packages pyswip
+pip install --user --break-system-packages pyswip vcstool
+vcs import --recursive --input dependencies.repos ../
